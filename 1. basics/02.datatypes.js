94daec674a1309f749370d529082551d3351ee10
chitransh992 <105778129+chitransh992@users.noreply.github.com>
2023-09-19 10:18:29 +0000

    conversion of datatypes

---
+++ 1. basics/02.datatypes.js
@@ -0,0 +1,6 @@
+"use strict" //treat all the JS code as a newer version
+
+//alert(3+3) // we are using nodejs, not browser so alert will not work here
+
+console.log(typeof null)
+console.log(typeof undefined)
---
+++ 1. basics/03.conversionOperation.js
@@ -0,0 +1,25 @@
+//"33" = 33
+//"33A" = "NAN" not a number conversion
+
+let score = "33"
+
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+//for boolean conversion
+
+//for empty string = false
+//for non empty string = true
+
+let isLoggedIn = ""
+
+let booleanLoggedIn = Boolean(isLoggedIn)
+console.log(booleanLoggedIn)
+
+
+
+
