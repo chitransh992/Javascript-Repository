4640ca005b50e1fa74cedc0e6c2ab20017a10214
chitransh992 <105778129+chitransh992@users.noreply.github.com>
2023-09-19 10:05:06 +0000

    Container config done

---
+++ 1. basics/variables.js
@@ -0,0 +1,8 @@
+const accountId =3322
+let accountEmail = "sdk@gmail.com"
+var accountPassword = "1234"
+accountCity = "Jaipur"
+
+// not use var because it leads to value scope or functional scope problem
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
