be69475053655d28e6dcce56b10c0af338b0ec78
chitransh992 <105778129+chitransh992@users.noreply.github.com>
2023-09-25 07:54:31 +0000

    iterators committed

---
+++ 5.iterators/1.one.js
@@ -0,0 +1,23 @@
+// for loop
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+
+//while loop
+
+let arr = 0;
+while(arr<=10){
+    console.log(arr);
+    arr = arr+1;
+}
+
+//Do while
+
+let score = 11
+
+do{
+    console.log('Score is '+score)
+    score++
+}while(score<=10)
