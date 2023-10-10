036b61201e9a1c9d9142912fa5818560317fa983
chitransh992 <105778129+chitransh992@users.noreply.github.com>
2023-10-10 16:15:51 +0530

    To Do List

---
+++ 7. Projects/To Do List/script.js
@@ -0,0 +1,36 @@
+const inputBox = document.getElementById("input-box")
+const listContainer = document.getElementById("list-container")
+
+
+function addTask(){
+    if(inputBox === ''){
+        alert("Please Write Task")
+    }
+    else{
+        let li = document.createElement("li")
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox = "";
+    savedData();
+}
+listContainer.addEventListener('click',function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked")
+        savedData();
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        savedData();
+    }
+},false)
+
+function savedData(){
+    localStorage.setItem("data",listContainer.innerHTML)
+}
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data")
+}
